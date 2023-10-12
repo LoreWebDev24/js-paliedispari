9aa4be14cb554b165022aca699e857be60f572a2
LoreWebDev24 <lorenzo.delre12@gmail.com>
2023-10-12 17:57:59 +0200

    add an idea for "pari e dispari"

--- js/master.js
+++ js/master.js
@@ -1,3 +1,34 @@
 // PARI E DISPARI 
 
-let pariORDispari = parseIntprompt
+let pariORDispari = prompt('Scegli fra pari o dispari')
+
+let userNumber = parseInt(prompt('Inserisci un numero:'))
+
+let botNumber = Math.floor(Math.random() * 5);
+console.log(botNumber)
+
+let somma = userNumber + botNumber 
+
+
+
+
+function checkPari(somma) {
+    let isPari 
+    if (somma % 2 != 0 ) {
+        isPari = false
+    } 
+
+    return isPari 
+}
+
+let checked = checkPari(somma)
+
+if (checked == true && pariORDispari == 'pari') {
+    alert ('Hai vinto')
+} else if (checked == true && pariORDispari == 'dispari') {
+    alert ('Hai perso')
+}else if (checked == false && pariORDispari == 'dispari' ) {
+    alert ('Hai vinto')
+}else {
+    alert ('Hai perso')
+}
